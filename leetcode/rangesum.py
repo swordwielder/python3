2acf6653891ebdc6623ab4ad56d7a2695726330a
swordwielder <qchen125@gmail.com>
2019-06-20 18:20:07 -0400

    source python env

---
+++ leetcode/rangesum.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
+        ans = 0
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                if L <= node.val <= R:
+                    ans += node.val
+                if L < node.val:
+                    stack.append(node.left)
+                if node.val < R:
+                    stack.append(node.right)
+        return ans
+        
+        
+        
+
