36cbd64ac2a39586e98544922b0d93bd9397caa0
swordwielder <qchen125@gmail.com>
2019-06-24 21:35:38 -0400

    submitting runnerup

--- hackerrank/runnerup.py
+++ hackerrank/runnerup.py
@@ -1,5 +1,20 @@
+import collections
 if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
 
-    print arr
+    array = list(arr)
+    maximum = min(array)
+    max2= min(array)
+    for c in array:
+        if maximum < c:
+            #print(c)
+            max2 = maximum
+            maximum = c
+        else:
+            if c > max2 and maximum>c:
+                max2=c
+    print (max2)
+
+    
+
