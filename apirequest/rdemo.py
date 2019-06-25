9bfb93bfefbfab88275a67f18cf29a7e5cc2ffac
swordwielder <qchen125@gmail.com>
2019-06-25 19:22:45 -0400

    adding comic

--- apirequest/rdemo.py
+++ apirequest/rdemo.py
@@ -1,10 +1,17 @@
 import requests
 
+li = ["r.status_code","r.ok","r.header","r.content","r.text","requests.get/requests.post vs params/data","r_dict['form']"]
+
+
+r= requests.get('http://httpbin.org/delay/2' , timeout=3)
+
+print (r)
+
+
+
 r= requests.get('https://imgs.xkcd.com/comics/python.png')
+with open('comic.png','wb') as f:
+	f.write(r.content)
 
 
 
-
-print (r.status_code)
-print (r.ok)
-print (r.headers)
