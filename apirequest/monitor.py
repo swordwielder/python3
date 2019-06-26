f46e3ae3730ef0cb2e92b4ac588677ee99ef2491
swordwielder <qchen125@gmail.com>
2019-06-26 14:32:15 -0400

    removing credentials

--- apirequest/monitor.py
+++ apirequest/monitor.py
@@ -2,18 +2,18 @@ import smtplib
 import requests
 
 r = requests.get('http://chenpost.com/', timeout=5)
-Email_address = 'qifenchen@gmail.com'
-Email_password = 'Well1170*'
+Email_address = os.environ.get('DB_USER')
+Email_password = os.environ.get('DB_PASS')
 
-if r.status_code != 200:
-    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-        smtp.ehlo()
-        smtp.starttls()
-        smtp.ehlo()
+# if r.status_code != 200:
+with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
 
-        smtp.login(Email_address, Email_password)
+    smtp.login(Email_address, Email_password)
 
-        subject = 'YOUR SITE IS DOWN!'
-        body = 'Restart the server and make sure it is back up'
-        msg = (f"Subject: {subject}\n\n{body}")
+    subject = 'Check chenpost website'
+    body = 'Restart the server and make sure it is back up'
+    msg = (f"Subject: {subject}\n\n{body}")
     smtp.sendmail(Email_address, 'qchen125@gmail.com', msg)
