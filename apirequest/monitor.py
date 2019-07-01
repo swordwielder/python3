d6071a44bb1bebbf7cdc653cd7cc754b0e8223a4
swordwielder <qchen125@gmail.com>
2019-07-01 13:02:29 -0400

    adding functions to python and crontab -e/-l

--- apirequest/monitor.py
+++ apirequest/monitor.py
@@ -2,19 +2,30 @@ import smtplib
 import os
 import requests
 
-r = requests.get('http://chenpost.com/', timeout=5)
 Email_address = os.environ.get('DB_USER')
 Email_password = os.environ.get('DB_PASS')
+Token = os.environ.get('TOKEN')
 
-# if r.status_code != 200:
-with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.ehlo()
+def notify_user():
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+        smtp.login(Email_address, Email_password)
+        subject = 'Check chenpost website'
+        body = 'Restart the server and make sure it is back up'
+        msg = (f"Subject: {subject}\n\n{body}")
+        smtp.sendmail(Email_address, 'qchen125@gmail.com', msg)
 
-    smtp.login(Email_address, Email_password)
+def reboot_server():
+    #look up aws route 53 api
+    print("rootboot the aws server")
 
-    subject = 'Check chenpost website'
-    body = 'Restart the server and make sure it is back up'
-    msg = (f"Subject: {subject}\n\n{body}")
-    smtp.sendmail(Email_address, 'qchen125@gmail.com', msg)
+try:
+    r = requests.get('http://chenpost.com/', timeout=5)
+    if r.status_code != 200:
+        notify_user()
+        reboot_server()
+except Exception as e:
+    notify_user()
+    reboot_server()
