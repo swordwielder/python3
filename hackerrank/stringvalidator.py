bbdc39d55886e361987424311ff40e0534b73549
swordwielder <qchen125@gmail.com>
2019-07-04 00:34:50 -0400

    adding a simplified version

--- hackerrank/stringvalidator.py
+++ hackerrank/stringvalidator.py
@@ -31,4 +31,12 @@ if __name__ == '__main__':
     print (lowercase(s))
     print (uppercase(s))
 
+#simplified
+if __name__ == '__main__':
+    str = input()
+    print (any(c.isalnum() for c in str))
+    print (any(c.isalpha() for c in str))
+    print (any(c.isdigit() for c in str))
+    print (any(c.islower() for c in str))
+    print (any(c.isupper() for c in str))
 
