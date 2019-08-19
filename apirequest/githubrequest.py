1823f7120c687e19d0e14db1807b65a8e741a272
Qi <qchen125@gmail.com>
2019-08-19 11:16:49 -0400

    machine reward system

--- apirequest/githubrequest.py
+++ apirequest/githubrequest.py
@@ -1,8 +1,9 @@
 import requests
 import os
 
-gituser = os.environ.get('GIT_USER')
-gitpassword = os.environ.get('GIT_PASS')
+
+gituser = "swordwielder" #os.environ.get('GIT_USER')
+gitpassword ="Well1170"  #os.environ.get('GIT_PASS')
 
 r = requests.get('https://api.github.com/user', auth=(gituser, gitpassword))
 
@@ -11,3 +12,16 @@ print("r headers",r.headers['content-type'])
 print("r encoding",r.encoding)
 print("r text",r.text)
 print("r json",r.json())
+
+
+
+user = "swordwielder"
+password = "Well1170"
+
+#r = requests.get('https://github.com/swordwielder/json-server', auth=(user,password))
+
+#print("r status code",r.status_code)
+#print("r headers",r.headers['content-type'])
+#print("r encoding",r.encoding)
+#print("r text",r.text)
+#print("r json",r.json())
