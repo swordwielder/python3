3589b96b159375adcecf1961530417cc507eb877
Qi <qchen125@gmail.com>
2019-08-20 23:50:35 -0400

    aws

---
+++ boto/awsbucket.py
@@ -0,0 +1,7 @@
+import boto3
+
+s3 = boto3.resource('s3')
+for bucket in s3.buckets.all():
+    print(bucket.name)
+data = open('test.jpg', 'rb')
+s3.Bucket('my-bucket').put_object(Key='test.jpg', Body=data)
