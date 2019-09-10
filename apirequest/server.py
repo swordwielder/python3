5d825603a97455b4cd4e801b0e9e95f1cdf94779
Qi <qchen125@gmail.com>
2019-09-10 15:10:52 -0400

    adding REST API implementation

---
+++ apirequest/server.py
@@ -0,0 +1,128 @@
+import json, os
+from flask import Flask, request, render_template,jsonify
+
+
+app = Flask(__name__)
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/team', methods=['PUT','GET'])
+def displayteam():
+    if request.method=='GET':
+        return jsonify(teams)
+
+    if request.method=='PUT':
+        return None
+
+@app.route('/team/<int:id>', methods=['PUT','GET'])
+def addtoteam(id):
+    newcity = request.args.get('city')
+    if request.method=='GET':
+        for team in teams:
+            if team['id']==id:
+                return jsonify(team)
+    if request.method=='PUT':
+        if newcity:
+            for team in teams:
+                team['city']=newcity
+                return jsonify(team)
+
+
+@app.route('/player',methods=['PUT','GET'])
+def displayplayer():
+    if request.method=='GET':
+        return jsonify(players)
+    if request.method=='PUT':
+        return None
+
+@app.route('/player/<int:id>',methods=['PUT','GET'])
+def addtoplayer(id):
+    newplayer = request.args.get('name')
+    if request.method=='GET':
+        for player in players:
+            if player['id']==id:
+                return jsonify(player)
+    if request.method=='PUT':
+        return None
+
+
+class Team:
+    def __init__(self,id,name,city,fullname,abbrev):
+        self.id=id
+        self.name=name
+        self.city=city
+        self.fullname=fullname
+        self.abbrev=abbrev
+
+    def __str__(self):
+        return f'{self.id},{self.name},{self.city},{self.fullname},{self.abbrev}'
+
+
+class Player:
+    def __init__(self,id,name,team_id):
+        self.id=id
+        self.name=name
+        self.team_id=team_id
+
+    def __str__(self):
+        return f'{self.id},{self.name},{self.team_id}'
+
+class Game:
+    def __init__(self,id,home_team_id,away_team_id,date):
+        self.id=id
+        self.home_team_id=home_team_id
+        self.away_team_id=away_team_id
+
+    def __str__(self):
+        return f'{self.id},{self.home_team_id},{self.away_team_id}'
+
+class Player_Stats:
+    def __init__(self,id,game_id,player_id,team_id,points,assists,rebounds,nerd):
+        self.id=id
+        self.game_id=game_id
+        self.player_id=player_id
+        self.team_id=team_id
+        self.points=points
+        self.assists=assists
+        self.rebounds=rebounds
+        self.nerd=nerd
+
+    def __str__(self):
+        return f'{self.id},{self.game},{self.player_id},{self.team_id},{self.team_id},{self.points},{self.assists},{self.rebounds},{self.nerd}'
+
+class Game_State:
+    def __init__(self,id):
+        self.id=id
+
+    def __str__(self):
+        return f'{self.id}'
+
+
+with open(os.getcwd()+'/data.json' ,'r') as jsonfile:
+    data = jsonfile.read()
+    jsonfile.close()
+
+jsondata = json.loads(data)
+teams_list=jsondata['Teams']
+players_list=jsondata['Players']
+games_list=jsondata['Games']
+
+print("GAMES LIST", games_list)
+teams = []
+players=[]
+for team in teams_list:
+    teams.append(Team(team['id'],team['name'],team['city'],team['full_name'],team['abbrev']).__dict__)
+
+for player in players_list:
+    players.append(Player(player['id'],player['name'],player['team_id']).__dict__)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
