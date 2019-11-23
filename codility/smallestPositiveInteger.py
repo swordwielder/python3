9f6cbc81495f96b5681a09e25e11e54c0b1677b3
Qi <qchen125@gmail.com>
2019-11-22 20:28:32 -0500

    codility update

---
+++ codility/smallestPositiveInteger.py
@@ -0,0 +1,39 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(A):
+    # write your code in Python 3.6
+
+    num = 1
+    A.sort()
+    
+    if 1 not in A:
+        return 1
+    if A[len(A)-1] < 1:
+        return 1
+    else:
+        for i in range(len(A)):
+            if A[i] == num+1:
+                num+=1
+            elif A[i] > num+1:
+                return num+1
+        return num+1
+
+
+def solution(a):
+    # write your code in Python 3.6
+    A = set(a)
+    ans = 1
+    while ans in A:
+       ans += 1
+    return ans
+
+
+if __name__ == '__main__':
+    b = [1,3,6,4,1,2]
+
+    a = set(b)
+    c = map(b)
+    print ("set of b is ",a)
+    print ("map of b is ", c)
+    print ("smallest integer value", solution(b))
