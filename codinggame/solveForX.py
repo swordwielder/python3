7fce1e7e2a067e1a0f8830e5c8f345ec45e08b2b
qc <qchen125@gmail.com>
2020-09-07 14:16:30 -0400

    https://www.codingame.com/clashofcode/clash/report/13698918daff11d792312a11bb06067c62f7b3a

--- codinggame/solveForX.py
+++ codinggame/solveForX.py
@@ -1,4 +1,4 @@
-#sol1
+#solution 1
 s = input().split()
 t=int(s[len(s)-1])
 if '-' in s:
@@ -11,8 +11,9 @@ if '/' in s:
     t*=int(s[s.index('/')-1])
 print(int(t))
 
+#a * x + y = z
 
-#sol 2
+#solution 2
 from re import findall
 a,b,y = list(map(int,findall(r'\d+', input())))
 print(round((y-b)/a))
